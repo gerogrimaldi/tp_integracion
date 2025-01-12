ea73965734a4d5fbb193ae259af79663c940e9b0
Brian Gotte <bngotte@gmail.com>
2025-01-12 15:53:01 -0300

    Centralizada la conexión a la BD

--- model/conexion.php
+++ model/conexion.php
@@ -1,4 +1,11 @@
 <?php
-    //$conex = mysqli_connect("127.0.0.1", "root", "", "2024_grupo2");
-    $conex = mysqli_connect("fcytpa.uader.edu.ar", "2024_grupo2", "Grupo2_8964", "2024_grupo2");
-
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+try {
+    $this->mysqli = mysqli_connect("127.0.0.1", "root", "", "granjas");
+    //$conex = mysqli_connect("fcytpa.uader.edu.ar", "2024_grupo2", "Grupo2_8964", "2024_grupo2");
+    $this->mysqli->set_charset("utf8mb4");
+} catch (mysqli_sql_exception $e) {
+    error_log("Error de conexión a la base de datos: " . $e->getMessage());
+    header("Location: index.php?opt=error_db");
+    exit; 
+}
